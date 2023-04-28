2dcd83c044e7bb56da938d959e8fd741730c8d30
Manoj R <Manoj@BCTCL-DL5420526.bctchn.local>
2023-04-28 18:00:25 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import download from './img/download.jpg';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div class="navbar">
+      <link to ="/logo.svg">
+      </link>
+      <button class="homescr"> <img src={download} alt="sample image" /></button>
+  <a href="#Support">Support</a>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+  <div class="Features">
+    <button class="featuresbtn">Features 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="Features-content">
+      <a href="#Link 1">Pro level</a>
+      <a href="#Link 2">Resource</a>
+      <a href="#Link 3">Tutorial</a>
+      <a href="#Link 4">Animation</a>
+      <a href="#Link 5">Modules</a>
     </div>
-  );
+  </div>
+  <a href="#Templates">Templates</a> 
+  <a href="#Blogs">Blogs</a>
+  <a href="#Home">Home</a>
+</div>
+     {/* < div class="para">
+ <a href='#test'>test</a>
+    </div>
+    <p> this is the sample website home</p> */}
+    </div>
+  ); 
 }
+
+
 
 export default App;
